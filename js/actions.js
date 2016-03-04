abc592ac717a6ee2145982ddac70d3df14c40a4e
Leonardo Melendez <leogatgens@gmail.com>
2016-03-03 21:45:57 -0600

    ajuste
    cambios manejo de los .js

--- js/actions.js
+++ js/actions.js
@@ -2,12 +2,20 @@
 
 var actionsManager = (function actionsEFFI(){
 	'use strict';
+
+	var $index = null;
+	var $acerca  = null;
+	var $botonimagen = null;
+
 	var productsHTML = '';
 	var MAX_PRODUCTS = 12;
 	var current_page = 1;
-	var publicAPI = {  init : init};
-	var $index = null;
-	var $acerca  = null;
+	var publicAPI = {  
+		init : init,
+		loadData : loadData
+	};
+	var productsObject = null;
+	var showCategories  = null;
 
 
 	function handleClickIndex(e){
@@ -18,72 +26,61 @@ var actionsManager = (function actionsEFFI(){
 		window.location = "acerca-de.html";	
 	}
 
+	function EventoMouserOver(e) {
+        this.style.cursor =  "pointer";
+    }
 	function init(params){
 
-	 $index = $(params.index); 
-	 $acerca = $(params.acerca); 
+		$index = $(params.index); 
+		$acerca = $(params.acerca); 
+		$botonimagen = $(params.botonCategoria);
 
-	$index.bind('click',handleClickIndex);
-	$acerca.bind('click',handleClickAcerca);
-
-
-	
-	
-	//Objeton Json con información de los productos a desplegar
-	var productsObject = {
-		"products": [
-			{ "imgURL":"images/canastas/canasta_1.jpg" , "name":"Canasta tipo 1", "price":"8000" }, 
-			{ "imgURL":"images/canastas/canasta_2.jpg" , "name":"Canasta tipo 2", "price":"9500" }, 
-			{ "imgURL":"images/canastas/canasta_3.jpg" , "name":"Canasta tipo 3", "price":"12000" },
-			{ "imgURL":"images/canastas/canasta_4.jpg" , "name":"Canasta tipo 4", "price":"7500" }, 
-			{ "imgURL":"images/canastas/canasta_5.jpg" , "name":"Canasta tipo 5", "price":"12000" },
-			{ "imgURL":"images/canastas/canasta_6.jpg" , "name":"Canasta tipo 6", "price":"16000" },
-			{ "imgURL":"images/canastas/canasta_7.jpg" , "name":"Canasta tipo 7", "price":"8000" },
-			{ "imgURL":"images/canastas/canasta_2.jpg" , "name":"Canasta tipo 8", "price":"5000" },
-			{ "imgURL":"images/canastas/canasta_5.jpg" , "name":"Canasta tipo 9", "price":"18000" }
-		]
-		};
-	
-	//Este for recorre todo lo que hay en el objeto y lo va sumando a una variable string para luego agregarse en products_wraper
-	for(var i=0; i < productsObject.products.length; i++)
-	{
-		productsHTML += '<div class="product">';
-        productsHTML +=	'<img src="' + productsObject.products[i].imgURL + '" />';
-		productsHTML +=	'<h3 class="product_name">' + productsObject.products[i].name + '</h3>';
-		productsHTML += '<h4 class="product_price">Precio: <span>&#8353; ' + productsObject.products[i].price + '</span></h4>';
-		productsHTML += '<div class="addToCart_btn"></div>';
-		productsHTML += '</div>';
-	}
-	
-	//Se agrega el html con los productos al div contenedor
-	$('#products_wraper').html(productsHTML);
+		$index.bind('click',handleClickIndex);
+		$acerca.bind('click',handleClickAcerca);
 		
-	current_page = getCurrentPage();
-	var showCategories = true;
+		$botonimagen.bind('click',MostraCategorias);
+		$botonimagen.bind('mouseover',EventoMouserOver);
+		
+		//Este for recorre todo lo que hay en el objeto y lo va sumando a una variable string para luego agregarse en products_wraper
+		for(var i=0; i < productsObject.products.length; i++)
+		{
+			productsHTML += '<div class="product">';
+	        productsHTML +=	'<img src="' + productsObject.products[i].imgURL + '" />';
+			productsHTML +=	'<h3 class="product_name">' + productsObject.products[i].name + '</h3>';
+			productsHTML += '<h4 class="product_price">Precio: <span>&#8353; ' + productsObject.products[i].price + '</span></h4>';
+			productsHTML += '<div class="addToCart_btn"></div>';
+			productsHTML += '</div>';
+		}
+		
+		//Se agrega el html con los productos al div contenedor
+		$('#products_wraper').html(productsHTML);
+			
+		current_page = getCurrentPage();
+		 showCategories = true;
 
+
+	
 
 	}	
 
-	function function_name(argument) {
-			$("#categorie_btn img").click( function(e){
-			if(showCategories)
-			{
-				$("#categories_menu").hide();
-				showCategories = false;
-			}
-			else
-			{
-				$("#categories_menu").show();
-				showCategories = true;
-			}
-		});
-	}
+function MostraCategorias(argument) {
+	alert('dhkdhkjhdkjhd');
+	if(showCategories)
+		{
+			$("#categories_menu").hide();
+			showCategories = false;
+		}
+		else
+		{
+			$("#categories_menu").show();
+			showCategories = true;
+		}
+}
+
 	
 	
-	$("#categorie_btn img").mouseover(function(e) {
-        this.style.cursor = "pointer";
-    });
 	
+
 	function handleMenu()
 	{
 		switch(current_page)
@@ -99,12 +96,18 @@ var actionsManager = (function actionsEFFI(){
 		}	
 	}
 	
-	
+
 	
 	function getCurrentPage() {
 		var loc = window.location;
 		var pathName = loc.pathname.substring(loc.pathname.lastIndexOf('/') + 1,loc.href.length);
 		return pathName;
+	}
+
+
+	function loadData(data){
+		//Objeton Json con información de los productos a desplegar
+		productsObject = data;
 	}
 	
 	return publicAPI;
--- js/main.js
+++ js/main.js
@@ -1,15 +1,28 @@
 
+var productsObject = {
+	"products": [
+		{ "imgURL":"images/canastas/canasta_1.jpg" , "name":"Canasta tipo 1", "price":"8000" }, 
+		{ "imgURL":"images/canastas/canasta_2.jpg" , "name":"Canasta tipo 2", "price":"9500" }, 
+		{ "imgURL":"images/canastas/canasta_3.jpg" , "name":"Canasta tipo 3", "price":"12000" },
+		{ "imgURL":"images/canastas/canasta_4.jpg" , "name":"Canasta tipo 4", "price":"7500" }, 
+		{ "imgURL":"images/canastas/canasta_5.jpg" , "name":"Canasta tipo 5", "price":"12000" },
+		{ "imgURL":"images/canastas/canasta_6.jpg" , "name":"Canasta tipo 6", "price":"16000" },
+		{ "imgURL":"images/canastas/canasta_7.jpg" , "name":"Canasta tipo 7", "price":"8000" },
+		{ "imgURL":"images/canastas/canasta_2.jpg" , "name":"Canasta tipo 8", "price":"5000" },
+		{ "imgURL":"images/canastas/canasta_5.jpg" , "name":"Canasta tipo 9", "price":"18000" }
+	]
+};
+actionsManager.loadData(productsObject);
 
- $(document).ready(
+ $(document).ready(	
 
 	function(){
 		actionsManager.init({
 			index : '#inicio',
-			acerca : '#acerca'
+			acerca : '#acerca',
+			botonCategoria : '#categorie_btn img'
 	});
 
 
+
 });
-// $( document ).ready(function() {
-//     console.log( "ready!" );
-// });
